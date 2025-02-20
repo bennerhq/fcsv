cd7904f46a73dd6de56efd65c8d3fd028998e0c4
Jens Kaas Benner <jens@bennerhq.com>
2025-02-20 21:57:19 +0100

    Update BNF grammar documentation for expression parsing to clarify filtering string structure

--- hdr/expr.h
+++ hdr/expr.h
@@ -1,25 +1,18 @@
 /*
-    BNF Grammar for expression parsing:
+    This is BNF grammar for the filtering string:
 
         <expression> ::= <conditional> | <arithmetic>
-
         <conditional> ::= <boolean> "?" <expression> ":" <expression>
-
         <boolean> ::= <arithmetic> <relop> <arithmetic>
-
         <relop> ::= ">" | "<" | "=" | ">=" | "<=" | "!="
 
         <arithmetic> ::= <term> | <arithmetic> ("+" | "-") <term>
-
         <term> ::= <factor> | <term> ("*" | "/") <factor>
-
         <factor> ::= <number> | "(" <expression> ")" | <variable>
-
         <number> ::= <digit> | <number> <digit>
-
         <digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
-
         <variable> ::= <letter> | <variable> <letter> | <variable> <digit>
+        <letter> ::= 'a' | 'b' | 'c' | ... | 'z' | 'A' | 'B' | 'C' | ... | 'Z'
 */
 #ifndef __EXPR_H__
 #define __EXPR_H__
