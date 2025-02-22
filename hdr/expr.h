79565139723360311d53bbcd28cb8c039517e587
Jens Kaas Benner <jens@bennerhq.com>
2025-02-22 23:11:43 +0100

    Update license revision in expr.h and enhance BNF grammar documentation for filtering strings

--- hdr/expr.h
+++ hdr/expr.h
@@ -1,6 +1,6 @@
 /**
  * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 43):
+ * "THE BEER-WARE LICENSE" (Revision 42):
  *
  * <jens@bennerhq.com> wrote this file.  As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
@@ -15,20 +15,20 @@
  */
 
 /**
-    This is BNF grammar for the csv filtering expression:
+    This is BNF grammar for the filtering string:
+ 
+        <expression> ::= <conditional> | <arithmetic>
+        <conditional> ::= <boolean> "?" <expression> ":" <expression>
+        <boolean> ::= <arithmetic> <relop> <arithmetic>
+        <relop> ::= ">" | "<" | "=" | ">=" | "<=" | "!="
 
-    <expression> ::= <conditional> | <arithmetic>
-    <conditional> ::= <boolean> "?" <expression> ":" <expression>
-    <boolean> ::= <arithmetic> <relop> <arithmetic>
-    <relop> ::= ">" | "<" | "=" | ">=" | "<=" | "!="
-
-    <arithmetic> ::= <term> | <arithmetic> ("+" | "-") <term>
-    <term> ::= <factor> | <term> ("*" | "/") <factor>
-    <factor> ::= <number> | "(" <expression> ")" | <variable>
-    <number> ::= <digit> | <number> <digit>
-    <digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
-    <variable> ::= <letter> | <variable> <letter> | <variable> <digit>
-    <letter> ::= 'a' | 'b' | 'c' | ... | 'z' | 'A' | 'B' | 'C' | ... | 'Z'
+        <arithmetic> ::= <term> | <arithmetic> ("+" | "-") <term>
+        <term> ::= <factor> | <term> ("*" | "/") <factor>
+        <factor> ::= <number> | "(" <expression> ")" | <variable>
+        <number> ::= <digit> | <number> <digit>
+        <digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+        <variable> ::= <letter> | <variable> <letter> | <variable> <digit>
+        <letter> ::= 'a' | 'b' | 'c' | ... | 'z' | 'A' | 'B' | 'C' | ... | 'Z'
 */
 #ifndef __EXPR_H__
 #define __EXPR_H__
